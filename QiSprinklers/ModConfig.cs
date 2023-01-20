4f7804912cff0e9f23c27fd56ebd26ff9b744e35
WhittenJ <JonathanWhitten@gmail.com>
2023-01-19 23:11:12 -0500

    Initial commit

---
+++ QiSprinklers/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiSprinklers
+{ 
+    public class ModConfig
+    {
+        public int SprinklerRange { get; set; } = 3;
+        public bool ActivateOnPlacement { get; set; } = true;
+        public bool ActivateOnAction { get; set; } = true;
+    }
+}
